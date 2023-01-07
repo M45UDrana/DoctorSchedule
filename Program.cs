cde5fb29fda2c3bddf759a9ecfae2b5ce7512eb4
ENOSISBD\masud <masud.rana@enosisbd.com>
2023-01-07 21:11:57 +0600

    Add Repository PlatformRepo.cs

--- Program.cs
+++ Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseInMemoryDatabase("InMem"));
 
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
